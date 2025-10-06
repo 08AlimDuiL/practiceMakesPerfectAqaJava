b8b9fcc575b35f34f5146fb83398abc5c2739d8f
Liudmila Portnova <portnovaliudmila@gmail.com>
2025-10-06 11:28:53 +0300

    Added testCreationContact test and ContactsData class. (#4)
    * Added the first test written using CKatalon.
    
    * GroupCreationTest has been changed. Part of the code has been moved to the BeforeMethod.
    
    * Refactor of the GroupCreationTest class and addition of the GroupData classÑ.
    
    * aAded ttstCreationContact test and ContactsData classÑ.

---
+++ practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/ContactsData.java
@@ -0,0 +1,114 @@
+package ru.stqa.ptf.addressbook;
+
+public class ContactsData {
+    private final String name;
+    private final String middleName;
+    private final String lastName;
+    private final String nickName;
+    private final String company;
+    private final String address;
+    private final String home;
+    private final String mobile;
+    private final String work;
+    private final String fax;
+    private final String email;
+    private final String birthDay;
+    private final String birthMonth;
+    private final String birthYear;
+    private final String group;
+
+    public ContactsData(
+            String name,
+            String middleName,
+            String lastName,
+            String nickName,
+            String company,
+            String address,
+            String homePhoneNumber,
+            String mobilePhoneNumber,
+            String work,
+            String fax,
+            String email,
+            String birthDay,
+            String birthMonth,
+
+            String birthYear,
+            String group) {
+
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.company = company;
+        this.address = address;
+        this.home = homePhoneNumber;
+        this.mobile = mobilePhoneNumber;
+        this.work = work;
+        this.fax = fax;
+        this.email = email;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhoneNumber() {
+        return home;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobile;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
--- practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTest.java
+++ practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTest.java
@@ -2,6 +2,8 @@ package ru.stqa.ptf.addressbook;
 
 import java.time.Duration;
 
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GroupCreationTest {
     private WebDriver wd;
+    private WebDriverWait wait;
 
 
     @BeforeMethod(alwaysRun = true)
@@ -16,9 +19,9 @@ public class GroupCreationTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        wait = new WebDriverWait(wd, Duration.ofSeconds(10));
         wd.get("http://127.0.0.1/addressbook/");
         login("admin", "secret");
-
     }
 
     private void login(String userName, String password) {
@@ -33,7 +36,7 @@ public class GroupCreationTest {
 
     @Test
     public void testGroupCreation() throws Exception {
-        gotoGroupPage();
+        goToGroupPage();
         initGroupCreation();
         fillGroupForm(new GroupData("test1", "test2", "test3"));
         submitGroupCreation();
@@ -41,12 +44,90 @@ public class GroupCreationTest {
         logout();
     }
 
+    @Test(dependsOnMethods = "testGroupCreation")
+    public void testCreationContact() throws Exception {
+        goToAddNewContact();
+        fillFormContact(new ContactsData(
+                "Vasilii",
+                "Ivanovich",
+                "Ivanov",
+                "Vasya",
+                "Neo",
+                "address",
+                "1112233",
+                "+79113334455",
+                "work",
+                "no",
+                "1112233@mail.ru",
+                "2",
+                "January",
+                "2000",
+                "test1")
+        );
+        goToHomePage();
+        logout();
+    }
+
+    private void fillFormContact(ContactsData contactsData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactsData.getName());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactsData.getMiddleName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactsData.getLastName());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(contactsData.getNickName());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactsData.getCompany());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactsData.getAddress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactsData.getHomePhoneNumber());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactsData.getMobilePhoneNumber());
+        wd.findElement(By.name("work")).click();
+        wd.findElement(By.name("work")).clear();
+        wd.findElement(By.name("work")).sendKeys(contactsData.getWork());
+        wd.findElement(By.name("fax")).click();
+        wd.findElement(By.name("fax")).clear();
+        wd.findElement(By.name("fax")).sendKeys(contactsData.getFax());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactsData.getEmail());
+        wd.findElement(By.name("bday")).click();
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactsData.getBirthDay());
+        wd.findElement(By.name("theform")).click();
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactsData.getBirthMonth());
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(contactsData.getBirthYear());
+        wd.findElement(By.name("new_group")).click();
+        new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactsData.getGroup());
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void goToHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void goToAddNewContact() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
     private void logout() {
         wd.findElement(By.linkText("Logout")).click();
     }
 
     private void returnToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.linkText("group page")).click();
     }
 
     private void submitGroupCreation() {
@@ -69,8 +150,8 @@ public class GroupCreationTest {
         wd.findElement(By.name("new")).click();
     }
 
-    private void gotoGroupPage() {
-        returnToGroupPage();
+    private void goToGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
     }
 
     @AfterMethod(alwaysRun = true)
