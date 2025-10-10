81f91b05315878e4a8a62c505ed7111f15102ac4
Portnova Liudmila <portnovaliudmila@gmail.com>
2025-10-10 17:33:09 +0300

    From lecture 4.9.

--- practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
+++ practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -30,25 +31,39 @@ public class GroupCreationTest extends TestBase {
 
     @Test
     public void testGroupCreationList() {
+
         app.getNavigationHelper().goToGroupPageHeader();
 
         List<GroupData> before = app.getGroupHelper().getGroupList();
 
-        GroupData group = new GroupData("test3", null, null);
+        GroupData group = new GroupData("test2", null, null);
         app.getGroupHelper().createGroup(group);
 
         List<GroupData> after = app.getGroupHelper().getGroupList();
 
         Assert.assertEquals(after.size(), before.size() + 1);
-
-        before.add(group);
+  /*
         int max = 0;
         for (GroupData g : after) {
-            if(g.getId() > max) {
+            if (g.getId() > max) {
                 max = g.getId();
             }
         }
-        group.setId(max);
+        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+            @Override
+            public int compare(GroupData o1, GroupData o2) {
+
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        };
+
+        Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        int max1 = after.stream().max(byId).get().getId();
+
+   */
+        int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+        group.setId(max1);
+        before.add(group);
 
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
