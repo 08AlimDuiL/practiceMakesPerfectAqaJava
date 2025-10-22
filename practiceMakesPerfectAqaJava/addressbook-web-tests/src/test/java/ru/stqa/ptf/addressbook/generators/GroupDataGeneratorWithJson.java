cf42e7c2c816a8aa55f5490052aa45357ffc28c6
Liudmila Portnova <portnovaliudmila@gmail.com>
2025-10-22 17:01:45 +0300

    From lecture 6.8 (#37)

--- practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/generators/GroupDataGeneratorWithJson.java
+++ practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/generators/GroupDataGeneratorWithJson.java
@@ -61,9 +61,13 @@ public class GroupDataGeneratorWithJson {
     private void saveAsJson(List<GroupData> groups, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(groups);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
+        ;
+//        Writer writer = new FileWriter(file);
+//        writer.write(json);
+//        writer.close();
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
