008bb21bbd2dc0b010157fdcd1865d2dfdea2177
Liudmila Portnova <portnovaliudmila@gmail.com>
2025-10-24 11:27:30 +0300

    From folder 7.1 (#42)

---
+++ practiceMakesPerfectAqaJava/addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbookMySql/model/GroupData.java
@@ -0,0 +1,109 @@
+package ru.stqa.ptf.addressbookMySql.model;
+
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+import java.util.Objects;
+
+@XStreamAlias("group")
+public class GroupData {
+    @XStreamOmitField
+    private int id = Integer.MAX_VALUE;
+
+    @Expose
+    private String name;
+    @Expose
+    private String header;
+    @Expose
+    private String footer;
+
+    public GroupData withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public GroupData withName(String name) {  //mutator
+        this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getHeader() {
+
+        return header;
+    }
+
+    public String getFooter() {
+
+        return footer;
+    }
+
+//    public GroupData(int id, String name, String header, String footer) {
+//        this.id = id;
+//        this.name = name;
+//        this.header = header;
+//        this.footer = footer;
+//    }
+//
+//    public GroupData(String name, String header, String footer) {
+//        this.id = Integer.MAX_VALUE;
+//        this.name = name;
+//        this.header = header;
+//        this.footer = footer;
+//    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id && Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name);
+    }
+
+    public boolean equalsByName(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+        //return  name != null ? name.equals(groupData.name) : groupData.name == null; старый способ
+    }
+
+    public int hashCodeByName() {
+
+        return Objects.hashCode(name);
+    }
+}
